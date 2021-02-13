d30ac849eae64e7b0dd67ed0f6999c8e0a902fbc
bilal1031 <bilalnaeem166@gmail.com>
2021-02-13 20:55:03 +0500

    Profile screen working completed

--- Componenets/ProfileOption.js
+++ Componenets/ProfileOption.js
@@ -1,2 +1,71 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Pressable } from "react-native";
+import { TouchableNativeFeedback } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/AntDesign";
+import color from "../Config/color";
+const ProfileOption = ({ icon, name, desc, colour }) => {
+  return (
+    <View style={styles.maindiv}>
+      <View style={styles.optiondiv}>
+        <TouchableNativeFeedback
+          style={[styles.optiondiv, { elevation: 0, flexDirection: "row" }]}
+        >
+          <View
+            style={{
+              width: 60,
+              height: 60,
+              backgroundColor: colour,
+              left: 15,
+              top: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: 35,
+            }}
+          >
+            <Icon name={icon} size={30} color={color.white} />
+          </View>
+          <View
+            style={{
+              //backgroundColor: color.grey,
+              flexDirection: "column",
+              left: 25,
+              top: 10,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>{name}</Text>
+            <Text style={{ fontSize: 14, color: color.grey, top: 5 }}>
+              {desc}
+            </Text>
+          </View>
+          <Icon
+            name="right"
+            size={25}
+            color={color.grey}
+            style={{ position: "absolute", right: 5, top: 26 }}
+          />
+        </TouchableNativeFeedback>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  maindiv: {
+    width: "100%",
+    height: 90,
+    // backgroundColor: color.grey,
+    alignItems: "center",
+    top: 20,
+    padding: 10,
+    overflow: "hidden",
+  },
+  optiondiv: {
+    width: "100%",
+    height: 80,
+    backgroundColor: color.white,
+    borderRadius: 20,
+    elevation: 1,
+    overflow: "hidden",
+  },
+});
+export default ProfileOption;
--- Componenets/ProfileScreen.js
+++ Componenets/ProfileScreen.js
@@ -4,6 +4,7 @@ import Navbar from "./NavBar";
 import ProfileCard from "./ProfileCard";
 import color from "../Config/color";
 import ProfileUlbutton from "./ProfileUlButton";
+import ProfileOption from "./ProfileOption";
 const ProfileScreen = ({ navigation }) => {
   return (
     <View>
@@ -27,6 +28,31 @@ const ProfileScreen = ({ navigation }) => {
             <ProfileUlbutton icon="message1" name="Messages" />
             <ProfileUlbutton icon="customerservice" name="Service" />
           </View>
+          <ProfileOption
+            icon="home"
+            name="Address"
+            desc="Ensure your harvesting address"
+            colour="#8c7aee"
+          />
+          <ProfileOption
+            icon="lock"
+            name="Privacy"
+            desc="System permission change"
+            colour="#f468b7"
+          />
+          <ProfileOption
+            icon="setting"
+            name="General"
+            desc="Basic functional settings"
+            colour="#ffc85a"
+          />
+          <ProfileOption
+            icon="bells"
+            name="Notification"
+            desc="Take over the news in time"
+            colour="#5cd1d3"
+          />
+          <View style={{ height: 100, width: "100%" }}></View>
         </ScrollView>
       </View>
       <Navbar navigation={navigation} />
